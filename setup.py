776c98b86f904cc7344d131df17a0e4638d6b1bd
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-08-19 12:39:59 +0200

    translations autocompilation

--- setup.py
+++ setup.py
@@ -16,13 +16,20 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import sys
 from setuptools import setup
+from django.core import management
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+currentdir = os.getcwd()
+os.chdir(os.path.join(currentdir, 'forum'))
+management.call_command('compilemessages', stdout=sys.stdout)
+os.chdir(currentdir)
 
 setup(
     name='django-paiji-forum',
