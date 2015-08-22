0104ddb54d06067159887862bd105d3423421c4d
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-08-22 15:04:52 +0200

    ignores egg-info

---
+++ testsettings.py
@@ -0,0 +1,36 @@
+import django.conf.global_settings as DEFAULT_SETTINGS
+
+
+SECRET_KEY = 'oxkvjtm'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
+}
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'bootstrap3',
+    'paiji2_forum',
+)
+
+ROOT_URLCONF = 'paiji2_carpooling.urls'
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+LANGUAGE_CODE = 'en'
