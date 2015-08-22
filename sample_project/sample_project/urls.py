ae0c9d04c03c0578ec92db999221b63c03bf59cc
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-08-22 15:01:30 +0200

    pep8 compliance (sample_project)

--- sample_project/sample_project/urls.py
+++ sample_project/sample_project/urls.py
@@ -6,24 +6,25 @@
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # paiji2-forum is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
-
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^i18n/', include('django.conf.urls.i18n')),
-] + i18n_patterns('',
+] + i18n_patterns(
+    '',
     url(r'^', include('home.urls')),
     url(r'^', include('paiji2_forum.urls', namespace='forum')),
     url(r'^admin/', include(admin.site.urls)),
--- sample_project/sample_project/wsgi.py
+++ sample_project/sample_project/wsgi.py
@@ -6,12 +6,12 @@
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
-# 
+#
 # paiji2-forum is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -25,7 +25,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sample_project.settings")
-
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sample_project.settings")
 application = get_wsgi_application()
