af00960f575b15a0e5d003a9da62761b6b258dd9
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2016-09-26 17:44:34 +0200

    Makes the dev easier

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.urandom(2048)
+# SECRET_KEY = os.urandom(2048)
+SECRET_KEY = "change this in production"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
